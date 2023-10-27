ad3a08e26debf21498e6eb51bf2d696f7350e85c
ankit-120 <roy.ankit5008@gmail.com>
2023-10-27 08:10:56 +0530

    Initial commit

---
+++ passwordGenerator/App.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Form from './components/Form';
+
+const App = () => {
+    return (
+        <View>
+            <Form />
+        </View>
+    );
+};
+
+export default App;
